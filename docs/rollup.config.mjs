08792534d288c142504d280c22a05ea7d758acd3
Ferdinand Prantl <prantlf@gmail.com>
2022-12-15 11:44:49 +0100

    chore: Update the documentation page

---
+++ docs/rollup.config.mjs
@@ -0,0 +1,50 @@
+import resolve from '@rollup/plugin-node-resolve'
+import livereload from 'rollup-plugin-livereload'
+import postcss from 'rollup-plugin-postcss'
+import { minify } from 'rollup-plugin-swc-minify'
+import { spawn } from 'child_process'
+
+const watch = process.env.ROLLUP_WATCH
+
+export default [
+  {
+    input: 'js/page.js',
+    output: {
+      sourcemap: true,
+      format: 'iife',
+      file: 'js/page.min.js'
+    },
+    plugins: [
+      resolve(),
+      watch && serve(),
+      watch && livereload('.'),
+      !watch && minify({ output: { comments: false } })
+    ],
+    watch: { clearScreen: false }
+  },
+  {
+    input: 'css/page.css',
+    output: { file: 'css/page.min.css' },
+    plugins: [
+      postcss({
+        extract: true,
+        minimize: !watch,
+        sourceMap: true
+      })
+    ]
+  }
+]
+
+function serve () {
+  let started = false
+  return {
+    writeBundle () {
+      if (started) return
+      started = true
+      spawn('npm', ['run', 'start', '--', '--dev'], {
+        stdio: ['ignore', 'inherit', 'inherit'],
+        shell: true
+      })
+    }
+  }
+}
